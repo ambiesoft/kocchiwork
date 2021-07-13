ea4acc4e71c176e9b367631ec3c2556716d711e4
ambiesoft <ambiesoft.trueff@gmail.com>
2021-07-13 22:24:12 +0900

    add dummy

---
+++ kocchiwork/gitrev.h
@@ -0,0 +1,19 @@
+// DO NOT EDIT
+// This file is created and will be overwritten by distSolution.py.
+// DO NOT EDIT
+
+#ifndef GITREV_INCLUDED_
+#define GITREV_INCLUDED_
+
+#include <map>
+#include <string>
+namespace GITREV {
+	inline std::map<std::string, std::string> GetHashes()
+	{
+		std::map<std::string, std::string> ret{
+{"dummy","df3761229dee8220c878eea50a0a630205bc771b"},
+		};
+		return ret;
+	}
+}
+#endif
